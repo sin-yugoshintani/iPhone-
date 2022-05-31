63a2f32278d9d8c00e52ac2e81824923cb551192
sin <yuho.shin@gmail.com>
2022-06-01 06:49:03 +0900

    Initial Commit

---
+++ iPhoneリリース/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  iPhoneリリース
+//
+//  Created by 菊地英治 on 2022/05/30.
+//
+
+import UIKit
+import Eureka
+
+class ViewController: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        form +++ Section("Personal Info")
+        
+        <<< TextRow(){ row in
+            row.title = "Username"
+            row.placeholder = "Username"
+        }
+        
+            
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+
